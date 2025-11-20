948eae6ad14b23d92d5dba188d5ee59a59180c92
Seyed-Cj <seyedxcj@gmail.com>
2025-11-20 20:54:18 +0330

    logger middleware fix morgan

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import { errorHandler } from './middlewares/errorHandler';
+import logger from './middlewares/logger.middleware';
+import morgan from 'morgan';
 
 const app = express();
+
+export const morganStream = {
+  write(message: string) {
+    const trimmedMessage = message.trim();
+    logger.info(trimmedMessage);
+  }
+};
 
 app.use(express.json());
 
 app.use(errorHandler);
+app.use(morgan("combined", { stream: morganStream }));
 
 export default app;
